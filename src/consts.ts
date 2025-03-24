fda274bb409cd3f4248a029828e5f3219cfa8251
Josh Nussbaum <64871335+joshnussbaum89@users.noreply.github.com>
2025-03-24 13:49:18 -0500

    feat(app): add 404 page, global colors, copy updates

--- src/consts.ts
+++ src/consts.ts
@@ -3,7 +3,7 @@
 
 export const SITE_URL = 'https://example.com' // Replace with your actual site URL
 export const SITE_TITLE = 'Wave Land Starter'
-export const SITE_DESCRIPTION = 'A starter template for building a website with Waveland.'
+export const SITE_DESCRIPTION = 'A starter template for building a website with Wave Land.'
 export const SITE_NAVIGATION = [
   { title: 'About', href: '/about' },
   { title: 'Contact', href: '/contact' },
