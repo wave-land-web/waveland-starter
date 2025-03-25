ccceb688c2e9a2cc58e88f2edd4460aa5d26b9ed
Josh Nussbaum <64871335+joshnussbaum89@users.noreply.github.com>
2025-03-25 17:22:54 -0500

    fix(app): current page styling

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_URL = 'https://example.com' // Replace with your actual site URL
+export const SITE_URL = 'https://waveland-starter.netlify.app/' // Replace with your actual site URL
 export const SITE_TITLE = 'Wave Land Starter'
 export const SITE_DESCRIPTION = 'A starter template for building a website with Wave Land.'
 export const SITE_NAVIGATION = [
